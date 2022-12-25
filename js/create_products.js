f307ac1d4db2c5c854ba652377f6b30c84b84ad4
rinnebyakuya <96972604+rinnebyakuya@users.noreply.github.com>
2022-12-25 14:58:43 +0300

    Adding maincategory pulls

--- js/create_products.js
+++ js/create_products.js
@@ -1,11 +1,51 @@
+const mainmenuItems = document.querySelectorAll('.dropdown-item');
 const submenuItems = document.querySelectorAll('.submenu-item');
+
+mainmenuItems.forEach(item => {
+  item.addEventListener('click', event => {
+    const category = event.target.textContent;
+    const url = `http://127.0.0.1:8000/products/maincategory?main_category=${category}`;
+    console.log(url);
+    fetch(url, { method: 'GET' })
+    .then(response => response.json())
+    .then(data => {
+    console.log(data)
+    const container = document.createElement('div');
+    container.classList.add('product-container');
+
+    // Check if data.data is an array before iterating over it
+    if (Array.isArray(data.data)) {
+      data.data.forEach(product => {
+        while (container.firstChild) {
+          container.removeChild(container.firstChild);
+        }
+        const name = product.name;
+        const newPrice = product.new_price;
+        const productImage = product.product_image;
+
+        const nameElement = document.createElement('p');
+        nameElement.textContent = name;
+        container.appendChild(nameElement);
+
+        const priceElement = document.createElement('p');
+        priceElement.textContent = newPrice;
+        container.appendChild(priceElement);
+
+        const imageElement = document.createElement('img');
+        imageElement.src = productImage;
+        container.appendChild(imageElement);
+      });
+    }
+
+    document.body.appendChild(container);
+  });
+  });
+});
 
 submenuItems.forEach(item => {
   item.addEventListener('click', event => {
-    let subcategory = event.target.textContent;
-    // Modify the subcategory to match the format expected by the server
-    // subcategory = subcategory.replace("’", "'");
-    const url = `http://127.0.0.1:8000/products/subcategory?category=${subcategory}`;
+    const category = event.target.textContent;
+    url = `http://127.0.0.1:8000/products/subcategory?category=${category}`;
     console.log(url);
     fetch(url, { method: 'GET' })
     .then(response => response.json())
